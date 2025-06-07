88e456956274035d1aae85c59a9b015794d47120
Shiml_der_Gamer47 <161841886+ShimlDerGamer47@users.noreply.github.com>
2025-06-07 23:10:15 +0200

    twitch_vod_shout_out.js

--- twitch_vod_shout_out.js
+++ twitch_vod_shout_out.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const body = document.body;
   const twitchVodShoutOut = document.getElementById("twitchVodShoutOutId");
   const params = new URLSearchParams(window.location.search);
   const videoId = params.get("videoId");
 
   if (!videoId) {
     console.warn("Kein 'videoId' Parameter in der URL gefunden.");
+    body.innerHTML = `<div style="font-family: Arial, Helvetica, sans-serif; background: black; position: absolute; top: 50%; left: 50%; transform: translate(-50%, 50%); display: flex; align-items: center; align-content: center; justify-items: center; justify-content: center; border: 2px solid black; border-radius: 5px; padding: 5px 5px; margin: 10px 10px 10px 10px;">
+      <p style="display: flex; align-items: center; align-content: center; justify-items: center; justify-content: center; text-align: center; font-size: 25px; color: white;">
+        Bitte gib eine Video-ID an!<br>
+        ?videoId=1234&autoplay=true&muted=true
+      </p>
+    </div>`;
     return;
   }
 
@@ -27,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
   } else if (muted !== null) {
     muted = muted === "true";
   } else {
-    muted = false;
+    muted = true;
     localStorage.setItem("muted", muted);
   }
 
